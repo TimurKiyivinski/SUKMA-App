ac86931d92dbeadeed97533ce8b597b2ff8c0f91
Timur Kiyivinski <deathseclipse@gmail.com>
2016-06-14 23:41:47 +0800

    [ADD] TWBS & FontAwesome [ADD] Navbar [WIP] Homepage

--- client/main.js
+++ client/main.js
@@ -2,10 +2,16 @@ import { Blaze } from 'meteor/blaze'
 import { Template } from 'meteor/templating'
 import { Meteor } from 'meteor/meteor'
 
+import '../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js'
+
 import './main.html'
 
 //const api = 'http://sukmasarawak2016.my/v1'
 const api = 'http://192.168.1.72:8000/v1'
+
+if (Meteor.isCordova) {
+} else {
+}
 
 Meteor.startup(() => {
     const container = $('#container')[0]
